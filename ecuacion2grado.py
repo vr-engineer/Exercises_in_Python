9ba2b0fe02ee2d06c6a34a378f03070ca3e07547
vr-engineer <97484986+vr-engineer@users.noreply.github.com>
2022-02-04 23:59:25 +0100

    Update ecuacion2grado.py

--- ecuacion2grado.py
+++ ecuacion2grado.py
@@ -15,6 +15,10 @@ elif a!=0 and ((b**2)-(4*a*c))<0:         #Soluciones complejas
     print('soluciones complejas\n')
     print('x1=',-b/(2*a),'+',sqrt(-((b**2)-4*a*c))/(2*a),'j\n')
     print('x2=',-b/(2*a),'-',sqrt(-((b**2)-4*a*c))/(2*a),'j\n')
+elif a==0 and b==0 and c==0:
+    print('La ecuación tiene infinitas soluciones\n')
+elif a==0 and b==0:
+    print('La ecuación no tiene solución\n') 
 else:                                     #Solución como ecuación de primer orden           
     x=-c/b
     print('x=',x,'\n')
